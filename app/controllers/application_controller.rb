413eb00353844853fd644f051bd32d7bba6c605b
Lucie Moreau <luciem.moreau@gmail.com>
2018-03-06 17:15:18 +0100

    devise/user views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: :home
+
+  def configure_permitted_parameters
+  # For additional in app/views/devise/registrations/edit.html.erb
+  devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :avatar])
+  end
 end
 
 #  test branch pk
