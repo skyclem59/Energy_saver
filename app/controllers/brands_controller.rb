21d8323b3c437f1e17ddb8d69b32f51ebf3ad204
Philippe Kerckove <Philippe.kerckove@fr.atlascopco.com>
2018-03-07 16:57:48 +0100

    add controllers

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,4 +1,5 @@
 class BrandsController < ApplicationController
   def index
+    Brand.all
   end
 end
--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,22 +1,46 @@
 class DevicesController < ApplicationController
   def index
-  end
+    before_action :set_device, only: [show:, :edit, :update, :destroy]
 
-  def show
-  end
+ #  list only the devices of the house vs current user
 
-  def new
-  end
+ @devices = Devices.where(house_id: current_user.house.id)
 
-  def create
+end
+
+def show
+end
+
+def new
+  @device = Device.new
+end
+
+def create
+  @device = Device.new(device_params)
+  @device[:house_id]= current_user.house.id
+  if @device.save
+  redirect_to devices_path(@horse)
   end
 
   def edit
   end
 
   def update
+    @device.update(device_params)
+        redirect_to devices_path
   end
 
   def destroy
+    @device.destroy
+        redirect_to devices_path
+  end
+
+  def set_device
+    @horse = Device.find(params[:id])
+  end
+
+  def device_params
+    params.require(:device).permit(:name, :type_id, :house_id)
   end
 end
+
