c9ca626f28d862f78f60ec0fefde5b491df9d46c
Lucie Moreau <luciem.moreau@gmail.com>
2018-03-12 16:12:46 +0100

    fix api commentees

--- app/services/smappee_service.rb
+++ app/services/smappee_service.rb
@@ -1,6 +1,6 @@
 # require 'oauth2'
 
-# client = OAuth2::Client.new('SCAILTEUX René', 'Pepsie59$', :site => 'https://app1pub.smappee.net/dev/v1/oauth2/token')
+# client = OAuth2::Client.new('', '', :site => 'https://app1pub.smappee.net/dev/v1/oauth2/token')
 
 # token = client.auth_code.authorize_url(:redirect_uri => 'http://localhost:3000/oauth2/callback')
 
@@ -9,12 +9,10 @@
 # # response = token.get('https://app1pub.smappee.net/dev/v1/oauth2/token')
 
 
-require 'rest-client'
-
-url = 'https://app1pub.smappee.net/dev/v1/oauth2/token'
-
-response = RestClient.post(url, { client_id: 'SCAILTEUX René', client_secret: 'Pepsie59$'}, headers={})
-
-p response
-
-# TODO
+# require 'rest-client'
+#
+# url = 'https://app1pub.smappee.net/dev/v1/oauth2/token'
+#
+# response = RestClient.post(url, { client_id: '', client_secret: ''}, headers={})
+#
+# p response
