8cfd3c9147ff78ce4140cd18f76a236ec85bf198
Lucie Moreau <34633244+sharrylucie@users.noreply.github.com>
2018-03-13 17:53:13 +0100

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,2 @@
-
-//import Chartkick from "chartkick";
-// window.Chartkick = Chartkick;
-
-// for Chart.js
-import Chart from "chart.js";
- window.Chart = Chart;
 import '../light.js';
 import '../thermostat.js';
