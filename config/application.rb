5337d7da4839a04bfcd7f6585e5c6e48e0194f5e
Lucie Moreau <luciem.moreau@gmail.com>
2018-03-13 15:49:29 +0100

    route ds scope locale

--- config/application.rb
+++ config/application.rb
@@ -23,4 +23,6 @@ module EnergySaver
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+  
+  config.i18n.default_locale = :en
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,16 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   # end
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :houses, only: [:show, :new, :create, :edit, :update]
-  resources :devices do
-    resources :types, only: :index
-  end
-  resources :actions, only: :index
-  resources :brands, only: :index
-  resources :consumptions , only: :show
+    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    scope '(:locale)', locale: /fr/ do
+    resources :houses, only: [:show, :new, :create, :edit, :update]
+    resources :devices do
+      resources :types, only: :index
+    end
+    resources :actions, only: :index
+    resources :brands, only: :index
+    resources :consumptions , only: :show
 
-  scope '(:locale)', locale: /fr/ do
-  root to: 'pages#home'
+    root to: 'pages#home'
+  end
 end
