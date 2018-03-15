ac7cb13da89c97f0675bad1fbe02bf4f6b96f5ee
Lucie Moreau <luciem.moreau@gmail.com>
2018-03-15 19:10:47 +0100

    nest on energy-saver callback

--- config/initializers/custom_config.rb
+++ config/initializers/custom_config.rb
@@ -2,7 +2,7 @@ PROVIDERS = [
   {
     name: 'Nest',
     image: 'nest.png',
-    link: "https://home.nest.com/login/oauth2?client_id=#{ENV['NEST_ID']}&redirect_uri=http://localhost:3000/nest/callback&state=EZAEREZAEZARZEREZZGSFGFSGSFGERZTAZ"
+    link: "https://home.nest.com/login/oauth2?client_id=#{ENV['NEST_ID']}&redirect_uri=https://www.energy-saver.info/nest/callback&state=EZAEREZAEZARZEREZZGSFGFSGSFGERZTAZ"
   },
   {
     name: 'Smappee',
