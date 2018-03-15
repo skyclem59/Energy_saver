8a3d463dff09d29191f4ba5afc159216a30e5d70
Lucie Moreau <luciem.moreau@gmail.com>
2018-03-15 19:16:12 +0100

    https

--- config/initializers/custom_config.rb
+++ config/initializers/custom_config.rb
@@ -2,7 +2,7 @@ PROVIDERS = [
   {
     name: 'Nest',
     image: 'nest.png',
-    link: "https://home.nest.com/login/oauth2?client_id=#{ENV['NEST_ID']}&redirect_uri=https://www.energy-saver.info/nest/callback&state=EZAEREZAEZARZEREZZGSFGFSGSFGERZTAZ"
+    link: "https://home.nest.com/login/oauth2?client_id=#{ENV['NEST_ID']}&redirect_uri=https://energy-saver-skyclem59.herokuapp.com/nest/callback&state=EZAEREZAEZARZEREZZGSFGFSGSFGERZTAZ"
   },
   {
     name: 'Smappee',
