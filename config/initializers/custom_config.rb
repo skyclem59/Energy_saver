2a9669eb8c0cc0a250f5bb02379bab63692042cc
clement scailteux <clement.scailteux@csreseaux.com>
2018-03-15 19:01:47 +0100

    final

--- config/initializers/custom_config.rb
+++ config/initializers/custom_config.rb
@@ -2,16 +2,19 @@ PROVIDERS = [
   {
     name: 'Nest',
     image: 'nest.png',
+    type: 'Thermostat',
     link: '/auth/nest'
   },
   {
     name: 'Smappee',
     image: 'smappee.jpg',
+    type: 'Smappee',
     link: '/auth/smappee'
   },
   {
     name: 'Philips HUE',
     image: 'hue.png',
+    type: 'Lampes',
     link: '/auth/philips'
   }
 ]
